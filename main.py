59905fab4acb0bda3e8deb2f8f0e453c219a9282
levyvix <levy.vix@gmail.com>
2024-05-02 00:07:25 -0300

    Add async functionality to main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+import time
+
+import anyio
+from asyncer import asyncify
+import asyncer
+
+
+def do_sync_work(name: str):
+    time.sleep(1)
+    return f"Hello, {name}"
+
+
+async def do_work(name: str):
+    await anyio.sleep(1)
+    return f"Hello, {name}"
+
+
+async def get_data():
+
+    async with asyncer.create_task_group() as task_group:
+        task_group.soonify(do_work)("Levy")
+        task_group.soonify(do_work)("Marques")
+        task_group.soonify(do_work)("Nunes")
+
+
+async def main():
+    await get_data()
+
+
+if __name__ == "__main__":
+    anyio.run(main)
